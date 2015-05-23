1310547f9df3f77269c42664a86c59029711417d
Screeder <luigipart@web.de>
2015-05-23 18:42:31 +0200

    Upload...

--- SPackages/SWards/Git.sh
+++
@@ -1,6 +0,0 @@
-git add -A
-git add -f SPackages/SWards/Resources/DLL/Evade.dll
-git add -f SPackages/SWards/Resources/DLL/LeagueSharp.GameFiles.dll
-git add -f SPackages/SWards/Resources/DLL/Newtonsoft.Json.dll
-git add -f SPackages/SWards/SWards.csproj.user
-git commit -am "Upload..."
